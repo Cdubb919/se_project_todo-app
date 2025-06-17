66e2bb12c85f78ca0797d8848d388a09b6216e6a
cdubb919 <christinanw919@gmail.com>
2025-06-16 23:43:56 -0500

    p7 corrections

---
+++ se_project_todo-app-main/pages/index.js
@@ -0,0 +1,58 @@
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+import { initialTodos, validationConfig } from "../utils/constants.js";
+import Todo from "../components/Todo.js";
+import FormValidator from '../components/FormValidator.js';
+
+const addTodoButton = document.querySelector(".button_action_add");
+const addTodoPopup = document.querySelector("#add-todo-popup");
+const addTodoForm = document.forms["add-todo-form"];
+const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
+const todosList = document.querySelector(".todos__list");
+
+const openModal = (modal) => {
+  modal.classList.add("popup_visible");
+};
+
+const closeModal = (modal) => {
+  modal.classList.remove("popup_visible");
+};
+
+const generateTodo = (data) => {
+  const todo = new Todo(data, "#todo-template");
+  const TodoElement = todo.getView();
+  return TodoElement;
+};
+
+const renderTodo = (item) => {
+  const todo = generateTodo(item);
+  todosList.append(todo);
+};
+
+addTodoButton.addEventListener("click", () => {
+  openModal(addTodoPopup);
+});
+
+addTodoCloseBtn.addEventListener("click", () => {
+  closeModal(addTodoPopup);
+});
+
+addTodoForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const name = evt.target.name.value;
+  const dateInput = evt.target.date.value;
+
+  const date = new Date(dateInput);
+  date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
+
+  const id = uuidv4();
+  const values = { name, date };
+  renderTodo(values);
+  closeModal(addTodoPopup);
+});
+
+initialTodos.forEach((item) => {
+  renderTodo(item);
+})
+
+const newFormValidator = new FormValidator(validationConfig, addTodoForm);
+newFormValidator.enableValidation();
